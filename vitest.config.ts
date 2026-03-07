decb9804b949e04c917ba978cc58bcf1efddef04
Gustavo Garcia <sousagustavogarcia@gmail.com>
2026-03-07 20:51:51 -0300

    feat: ajustes config ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 export default defineConfig({
 	test: {
 		environment: "node",
-		include: ["src/**/*.spec.ts"], // 👈 IMPORTANTE
+		include: ["src/**/*.spec.ts"],
 		exclude: ["dist", "node_modules"],
 	},
 	resolve: {
